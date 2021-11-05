28d2275e47d9f402d948a81e2503114891e6160d
Julen Garro <julengarro@opendeusto.es>
2021-11-05 10:05:02 +0100

    Problemaren ebazpena

--- examMatrix/main.cpp
+++ examMatrix/main.cpp
@@ -7,8 +7,33 @@
 
 #include <iostream>
 
+using namespace std;
+//Funtzio honetan zenbakiak inprimatzen dira
+void printNumber(int limit)
+{
+    string numPrint;
+    string result;
+    
+    for(int i=1; i<=limit;i++)
+    {
+        result = "";
+        for (int j=1;j<=i;j++)
+        {
+            numPrint = to_string(j);
+            result = result + numPrint + " ";
+        }
+        cout << result << endl;
+    }
+}
+
+//Erabiltzaileari zenbakia eskatu eta funtzioari deitzen dio
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    int limit;
+    
+    cout << "Escribe el límite que mostrará la matriz";
+    cin >> limit;
+    
+    printNumber(limit);
     return 0;
 }
